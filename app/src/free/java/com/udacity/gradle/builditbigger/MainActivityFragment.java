72ce92a3a1cab45cfd0c53335208e1583821e1cd
leskevicius <rokaslopas@gmail.com>
2017-10-09 16:42:48 -0400

    Added two different flavors. Free has ads, paid does not.

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,15 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-
+import com.udacity.gradle.builditbigger.R;
 
 /**
  * A placeholder fragment containing a simple view.
